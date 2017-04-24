9c5a6d3b34928479b8d658a1cf3ade714a44f0f2
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-24 07:56:23 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,12 +2,12 @@ class MicropostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   def create
-     @micropost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
